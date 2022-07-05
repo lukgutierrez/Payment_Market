798a7bad4ec09469fed734e40f5e158996e8ed30
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-07-05 11:57:51 -0300

    My number one commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Material App',
+        title: 'Payment Market',
         home: HomePage());
   }
 }
