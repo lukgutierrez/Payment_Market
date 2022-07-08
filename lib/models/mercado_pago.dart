b8fcaeab10b8985e03eda83db68eabcb615157bd
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-07-08 17:25:09 -0300

    problem solution

--- lib/models/mercado_pago.dart
+++ lib/models/mercado_pago.dart
@@ -25,82 +25,91 @@ class MercadoPago extends StatelessWidget {
           //ETAPA N°1
           Transferencia(),
           //CARGA DE DATOS
-          Column(
-            children: [
-              //DATO N° 1
-              Date(FechaActual),
-              Divider(
-                color: Colors.black,
-              ),
-              //DATO N° 2
-              Date(monto),
-              Divider(),
-              //DATO N° 3
-              Container(
-                child: ListTile(
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Para"),
-                      Divider(),
-                      Text(
-                        para,
-                        style: TextStyle(fontWeight: FontWeight.w700),
-                      ),
-                      Divider(),
-                      Text(banco)
-                    ],
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: Column(
+                children: [
+                  //DATO N° 1
+                  Date(FechaActual),
+                  Divider(
+                    color: Colors.black,
                   ),
-                ),
+                  //DATO N° 2
+                  Date(monto),
+                  Divider(),
+                  //DATO N° 3
+                  Container(
+                    child: ListTile(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Para"),
+                          Divider(),
+                          Text(
+                            para,
+                            style: TextStyle(fontWeight: FontWeight.w700),
+                          ),
+                          Divider(),
+                          Text(banco)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
 
           //BOTONES EN PANTALLAS
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //BOTON N°1
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                  ),
-                  onPressed: () {},
-                  child: Container(
-                    height: 45,
-                    width: 300.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Image(image: AssetImage("assets/share.png")),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Compartir comprobante"),
-                        ),
-                      ],
+          Container(
+            color: Colors.black,
+            height: 150.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //BOTON N°1
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
                     ),
-                  )),
-              //BOTON N° 2
-              Botones(
-                  Color(
-                    0xffE3F0F8,
-                  ),
-                  () {},
-                  "Hacer otra trasferencia"),
-              //BOTON N° 3
-              Botones(
-                  Color(
-                    0xffFFFFFF,
-                  ), () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              }, "Ir al inicio")
-            ],
+                    onPressed: () {},
+                    child: Container(
+                      height: 45,
+                      width: 300.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image(image: AssetImage("assets/share.png")),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Compartir comprobante"),
+                          ),
+                        ],
+                      ),
+                    )),
+                //BOTON N° 2
+                Botones(
+                    Color(
+                      0xffE3F0F8,
+                    ),
+                    () {},
+                    "Hacer otra trasferencia"),
+                //BOTON N° 3
+                Botones(
+                    Color(
+                      0xffFFFFFF,
+                    ), () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                }, "Ir al inicio")
+              ],
+            ),
           ),
         ],
       ),
@@ -113,7 +122,7 @@ Transferencia() {
   return Stack(
     children: [
       Container(
-        height: 75.0,
+        height: 80.0,
         color: Color(0xff00A650),
       ),
       Positioned(
